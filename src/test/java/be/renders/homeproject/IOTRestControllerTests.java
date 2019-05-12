87c33a12c37cf868f25dec93f0d34c92b7355856
jo.renders@gmail.com <jrke0402>
2019-05-12 21:57:43 +0200

    add first unittest

---
+++ src/test/java/be/renders/homeproject/IOTRestControllerTests.java
@@ -0,0 +1,34 @@
+package be.renders.homeproject;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IOTRestControllerTests {
+
+	@Spy
+	private MetingRepository metingRepository;
+
+	@InjectMocks
+	private IOTRestController iotRestController;
+
+	@Before
+	public void initMocks(){
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void registreerMetingGeeftCorrecteResponsTerug() {
+		Mockito.doReturn(ResponseCode.OK).when(metingRepository).registreerMeting(1L, 2.22);
+		Respons respons = iotRestController.registreerMeting(1L, 2.22);
+		assertEquals(ResponseCode.OK, respons.getResponsCode());
+		assertEquals("succes", respons.getResponsString());
+	}
+
+}
